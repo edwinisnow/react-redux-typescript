81a42bdc3641c1be8b941a76f6eb1e922e6db9d4
edwinisnow <edwin04@gmail.com>
2020-11-04 23:47:13 +0000

    App Props

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+interface AppProps {
+    color: string
+}
+
+class App extends Component<AppProps> {
+    render() {
+        return <div>Hi there {this.props.color}</div>
+    }
+}
+
+ReactDOM.render(<App color="red" />, document.getElementById('root'));
+
